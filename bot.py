f21b7616861e3fd9959c5d3defad69929a5a621d
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 19:58:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,2 @@
-import telebot
-
-bot = telebot.TeleBot("8559357103:AAGTeH5u4DiwDYZDBSDn4z1O7P3pBXwDse4")
-
-@bot.message_handler(content_types=['photo', 'document', 'text'])
-def test(message):
-    print("TYPE:", message.content_type)
-    bot.reply_to(message, message.content_type)
-
+bot.remove_webhook()
 bot.infinity_polling()
