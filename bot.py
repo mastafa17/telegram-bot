f0e31eb96694e880533846649ad613a4de89ad17
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 19:55:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
+import telebot
+
+bot = telebot.TeleBot("8559357103:AAGTeH5u4DiwDYZDBSDn4z1O7P3pBXwDse4")
+
 @bot.message_handler(content_types=['photo', 'document', 'text'])
 def test(message):
     print("TYPE:", message.content_type)
     bot.reply_to(message, message.content_type)
+
+bot.infinity_polling()
