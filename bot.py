680bd5f83a5925c7b5025dd96126aae4653beb07
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 20:27:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import telebot
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot("YOUR_TOKEN")
 
 print("BOT STARTED")
 
 @bot.message_handler(content_types=['photo'])
-def get_photo(message):
+def get_file_id(message):
+    print("PHOTO RECEIVED")
+
     file_id = message.photo[-1].file_id
-    print(file_id)
+    print("FILE ID:", file_id)
+
     bot.reply_to(message, file_id)
 
 bot.remove_webhook()
