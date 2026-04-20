40aa51f9668543a989f2e5848fe1775c673e7e88
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 22:04:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
 import telebot
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")  # أفضل من كتابته مباشرة
+
 bot = telebot.TeleBot(TOKEN)
 
-print("BOT IS RUNNING")
-print("BOT INFO:", bot.get_me())
+print("🔥 BOT STARTED")
 
-bot.delete_webhook()
+@bot.message_handler(content_types=['photo'])
+def photo_handler(message):
+    print("📸 PHOTO RECEIVED")
 
-@bot.message_handler(func=lambda m: True)
-def all_messages(message):
-    print("🔥 GOT MESSAGE:", message.text)
-    bot.reply_to(message, "OK")
+    file_id = message.photo[-1].file_id
+    print("FILE ID:", file_id)
 
-bot.infinity_polling()
+    bot.reply_to(message, file_id)
+
+bot.remove_webhook()
+
+print("🚀 STARTING POLLING")
+
+bot.infinity_polling(skip_pending=True)
