7e99e8e8c7becbba8fa1d97885ca9c7b8eb40961
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 20:52:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,16 @@
 import telebot
 import os
 
-# جلب التوكن من Railway Variables
-TOKEN = os.getenv("BOT_TOKEN")
-
-if TOKEN is None:
-    print("❌ BOT_TOKEN is missing!")
-    exit()
-
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
 print("BOT IS RUNNING")
 
-# اختبار استقبال أي رسالة
-@bot.message_handler(func=lambda message: True)
-def handle_all(message):
-    print("🔥 MESSAGE RECEIVED")
-    print("TYPE:", message.content_type)
+updates = bot.get_updates()
+print("UPDATES TEST:", updates)
 
-    if message.content_type == "photo":
-        file_id = message.photo[-1].file_id
-        print("📸 FILE ID:", file_id)
-        bot.reply_to(message, file_id)
-    else:
-        bot.reply_to(message, "ارسل صورة حتى أطلع file_id")
+@bot.message_handler(func=lambda m: True)
+def test(message):
+    print("🔥 GOT MESSAGE")
+    bot.reply_to(message, "OK")
 
-# تنظيف أي webhook قديم
-bot.remove_webhook()
-
-print("STARTING POLLING...")
-
-# تشغيل البوت
-bot.infinity_polling(skip_pending=True)
+bot.infinity_polling()
