60418b8cddb4c3d92701460fee2f87d0e78a2839
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 20:10:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,14 @@ import telebot
 
 bot = telebot.TeleBot("8559357103:AAGTeH5u4DiwDYZDBSDn4z1O7P3pBXwDse4")
 
-bot.delete_webhook()
-print("WEBHOOK DELETED")
+print("BOT STARTED")
+
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    print("PHOTO RECEIVED")
+    file_id = message.photo[-1].file_id
+    print("FILE ID:", file_id)
+    bot.reply_to(message, file_id)
+
+bot.remove_webhook()
+bot.infinity_polling()
