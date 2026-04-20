0f17a2d022eec9091ef36f9560377c3583decb7f
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 19:59:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,19 @@
+import telebot
+
+bot = telebot.TeleBot("8559357103:AAGTeH5u4DiwDYZDBSDn4z1O7P3pBXwDse4")
+
+@bot.message_handler(content_types=['photo', 'document', 'text'])
+def handler(message):
+    print("RECEIVED:", message.content_type)
+
+    if message.content_type == "photo":
+        file_id = message.photo[-1].file_id
+        print("FILE ID:", file_id)
+        bot.reply_to(message, file_id)
+    else:
+        bot.reply_to(message, message.content_type)
+
+# مهم جداً:
 bot.remove_webhook()
+
 bot.infinity_polling()
