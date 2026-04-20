4af656312803e28843220caefe32199c0fd4ed2b
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 19:26:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+from aiogram import F
+
+# هذا الكود مخصص فقط لاستخراج الـ IDs
 @dp.message(F.photo)
 async def get_photo_id(message: types.Message):
-    # هذا السطر سيعطيك الـ ID الخاص بالصورة
-    await message.answer(f"ID الصورة هو:\n`{message.photo[-1].file_id}`", parse_mode="MarkdownV2")
+    # نأخذ آخر صورة (تكون بأعلى دقة)
+    photo_id = message.photo[-1].file_id
+    await message.answer(f"كود الصورة (File ID) هو:\n\n`{photo_id}`", parse_mode="MarkdownV2")
