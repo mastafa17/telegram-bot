32f6641ffa56f0828d01c1f0a47ec75ed0560f99
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 22:05:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,16 @@
 import telebot
-import os
 
-TOKEN = os.getenv("BOT_TOKEN")  # أفضل من كتابته مباشرة
-
+TOKEN = "BOT_TOKEN"
 bot = telebot.TeleBot(TOKEN)
 
-print("🔥 BOT STARTED")
-
+# لما المستخدم يرسل صورة
 @bot.message_handler(content_types=['photo'])
-def photo_handler(message):
-    print("📸 PHOTO RECEIVED")
-
+def get_file_id(message):
+    # الصورة تجي بعدة أحجام، ناخذ أعلى جودة (آخر وحدة)
     file_id = message.photo[-1].file_id
-    print("FILE ID:", file_id)
+    
+    bot.reply_to(message, f"📸 File ID:\n{file_id}")
 
-    bot.reply_to(message, file_id)
-
-bot.remove_webhook()
-
-print("🚀 STARTING POLLING")
-
-bot.infinity_polling(skip_pending=True)
+# تشغيل البوت
+print("Bot is running...")
+bot.infinity_polling()
