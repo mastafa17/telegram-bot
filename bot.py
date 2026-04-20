fdd4d6c2f18972683dfdbb0598bed97d717eb8fb
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 20:29:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,19 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+print("BOT STARTED")
+
 @bot.message_handler(func=lambda m: True)
-def test(message):
-    print("MESSAGE TYPE:", message.content_type)
+def all_messages(message):
+    print("MESSAGE RECEIVED:", message.content_type)
     bot.reply_to(message, "OK")
+
+bot.remove_webhook()
+
+print("STARTING POLLING")
+
+bot.infinity_polling(skip_pending=True)
