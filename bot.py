ec827cca3f16bd16b3ddcb92dcaae69810fa56aa
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 19:53:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,4 @@
-import telebot
-
-bot = telebot.TeleBot("8559357103:AAGTeH5u4DiwDYZDBSDn4z1O7P3pBXwDse4")
-
-@bot.message_handler(content_types=['photo'])
-def get_file_id(message):
-    file_id = message.photo[-1].file_id
-    print("FILE ID:", file_id)
-    bot.reply_to(message, file_id)
-
-bot.infinity_polling()
+@bot.message_handler(content_types=['photo', 'document', 'text'])
+def test(message):
+    print("TYPE:", message.content_type)
+    bot.reply_to(message, message.content_type)
