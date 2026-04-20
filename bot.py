f2732a02c33e9bdc74ff378495465f661484940f
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-20 20:31:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,32 @@
 import telebot
 import os
 
+# جلب التوكن من Railway Variables
 TOKEN = os.getenv("BOT_TOKEN")
 
+# إنشاء البوت
 bot = telebot.TeleBot(TOKEN)
 
 print("BOT STARTED")
 
-@bot.message_handler(func=lambda m: True)
+# استقبال أي رسالة (للتأكد أنه شغال)
+@bot.message_handler(func=lambda message: True)
 def all_messages(message):
-    print("MESSAGE RECEIVED:", message.content_type)
-    bot.reply_to(message, "OK")
+    print("MESSAGE TYPE:", message.content_type)
 
+    # إذا صورة
+    if message.content_type == "photo":
+        file_id = message.photo[-1].file_id
+        print("FILE ID:", file_id)
+
+        bot.reply_to(message, f"FILE ID:\n{file_id}")
+    else:
+        bot.reply_to(message, "ارسل صورة حتى أطلع file_id")
+
+# تنظيف أي webhook قديم
 bot.remove_webhook()
 
-print("STARTING POLLING")
+print("STARTING POLLING...")
 
+# تشغيل البوت
 bot.infinity_polling(skip_pending=True)
-
-print("FILE IS RUNNING")
