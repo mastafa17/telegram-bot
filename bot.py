02a51f0e4d8e94b88d978dc7281d90403ee5e184
mastafa17 <mastafaalubaidy@gmail.com>
2026-04-21 20:00:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,9 +77,9 @@ def send_adhan(prayer):
     time_12 = to_12_hour(time_now)
 
     text = (
-    f"حان الآن مووووووووووووووعد صلاة {prayer} 🕌\n"
+    f"حان الآن موعد صلاة {prayer} 🕌\n"
     f"الوقت: {time_12} ⏰"
-            )
+    )
 
     try:
         bot.send_photo(CHAT_ID, images[prayer], caption=text)
